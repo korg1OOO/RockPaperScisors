f523f7c1fe912b64dae173697949867052449cc9
Enzo Michel Barbosa Feldman <enzomichelfeldman@gmail.com>
2023-08-21 17:44:28 -0300

    Adding loop and Break to switch case

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let computerSelection;
 let playerSelection;
+let computerScore = 0;
+let playerScore = 0;
 
 function getComputerChoice() {
     const computerChoices = ["Rock", "Paper", "Scissors"];
@@ -10,44 +12,57 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = prompt("Write your choice!");
     computerSelection = getComputerChoice()
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    console.log("Player: " + playerSelection + "\nMachine: " + computerSelection);
     switch (computerSelection) {
-        case rock:
-            if (playerSelection === scissors || playerSelection === "scissors" || playerSelection === "scissor") {
-                return "You Lose! Rock beats Scissors!";
-            } else if(playerSelection === rock || playerSelection === "rock" || playerSelection === "rocks") {
-                return "It was a draw!";
+        case "Rock":
+            if (playerSelection === "Scissors") {
+                console.log("You Lose! Rock beats Scissors!");
+                computerScore++;
+            } else if(playerSelection === "Rock" || playerSelection === "rock" || playerSelection === "rocks") {
+                console.log("It was a draw!");
             } else {
-                return "You win! Paper beats Rock!";
+                console.log("You win! Paper beats Rock!");
+                playerScore++;
             }
-        case paper:
-            if (playerSelection === scissors || playerSelection === "scissors" || playerSelection === "scissor") {
-                return "You win! Scissors beat Paper!";
+            break;
+        case "Paper":
+            if (playerSelection === "Scissors") {
+                console.log("You win! Scissors beat Paper!");
+                playerScore++;
             }
-            else if(playerSelection === paper || playerSelection === "paper" || playerSelection === "papers") {
-                return "It was a draw!";
-            }
-            else {
-                return "You Lose! Paper beats Rock!";
-            }
-        case scissors:
-            if (playerSelection === paper || playerSelection === "paper" || playerSelection === "papers") {
-                return "You lose! Scissors beats Paper!";
-            }
-            else if (playerSelection === scissors || playerSelection === "scissors" || playerSelection === "scissor") {
-                return "It was a draw!";
+            else if(playerSelection === "Paper") {
+                console.log("It was a draw!");
             }
             else {
-                return "You win! Rock beats Scissors!";
+                console.log("You Lose! Paper beats Rock!");
+                computerScore++;
             }
+            break;
+        case "Scissors":
+            if (playerSelection === "Paper" || playerSelection === "paper" || playerSelection === "papers") {
+                console.log("You lose! Scissors beats Paper!");
+                computerScore++;
+            }
+            else if (playerSelection === "Scissors" || playerSelection === "scissors" || playerSelection === "scissor") {
+                console.log("It was a draw!");
+            }
+            else {
+                console.log("You win! Rock beats Scissors!");
+                playerScore++;
+            }
+            break;
     }
 }
 
 function game(playRound) {
-    let round = 5;
-    while (round < 5) {
-        playRound(playerSelection, computerSelection);
-        console.log(playRound(playerSelection, computerSelection))
+    let round = 1;
+    while (round <= 5) {
+        console.log("Round " + round);
+        playRound();
+        console.log("Your score: " + playerScore + "\nMachine score: " + computerScore);
+        round++;
         }
     }
 
-    game();
+    game(playRound);
